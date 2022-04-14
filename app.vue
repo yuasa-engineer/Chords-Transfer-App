493493890c0e79381d80e703ae15db745e09903b
JoeyYuasa <yuasa@10-kai.jp>
2022-04-15 01:07:20 +0900

    update tailwind と prettierの導入

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+const tes: string = 'gahaha'
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <div class="asasa" v-if="true">aaa</div>
+    <p v-for="a in ['a', 'b', 'c']">
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      {{ a }}
+    </p>
+    <input v-model="tes" />
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  content: ['./**/*.vue'],
+  darkMode: 'media', // or 'media' or 'class'
+}
